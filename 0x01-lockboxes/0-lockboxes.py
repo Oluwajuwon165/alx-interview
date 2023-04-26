9c66a6d046670c2a508b598c2e564cc25a89abb8
Oluwajuwon165 <Oladitioluwajuwon165@gmail.com>
2023-04-26 15:40:51 +0100

    Satisfying pep8 requirements

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
 
+
 def canUnlockAll(boxes):
     """
     Determines if all the boxes can be opened.
     """
+
     n = len(boxes)
     keys = [False] * n
-    keys[0] = True # the first box is unlocked by default
-    stack = [0] # start with the first box
+    keys[0] = True  # the first box is unlocked by default
+    stack = [0]  # start with the first box
+
     while stack:
         box = stack.pop()
         for key in boxes[box]:
-            if key >= 0 and key < n and not keys[key]:
+            if 0 <= key < n and not keys[key]:
                 keys[key] = True
                 stack.append(key)
+
     return all(keys)
