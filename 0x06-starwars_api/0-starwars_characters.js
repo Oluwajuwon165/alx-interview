000d21b158aaabfbd8ad1df9502edcbb804a38c7
Oluwajuwon165 <Oladitioluwajuwon165@gmail.com>
2023-06-01 16:55:29 +0100

    Star Wars Characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,33 +1,23 @@
 #!/usr/bin/node
+
 const request = require('request');
 
 const movieId = process.argv[2];
 
-if (!movieId) {
-  console.error('Please provide a movie ID as a command-line argument.');
-  process.exit(1);
-}
-
-const url = `https://swapi.dev/api/films/${movieId}`;
+const url = `https://swapi.dev/api/films/${movieId}/`;
 
 request(url, (error, response, body) => {
   if (error) {
-    console.error('Error:', error);
-  } else if (response.statusCode !== 200) {
-    console.error('Invalid response:', response.statusCode);
+    console.error(error);
   } else {
-    const film = JSON.parse(body);
-    const characters = film.characters;
-
+    const characters = JSON.parse(body).characters;
     characters.forEach((characterUrl) => {
-      request(characterUrl, (error, response, body) => {
-        if (error) {
-          console.error('Error:', error);
-        } else if (response.statusCode !== 200) {
-          console.error('Invalid response:', response.statusCode);
+      request(characterUrl, (charError, charResponse, charBody) => {
+        if (charError) {
+          console.error(charError);
         } else {
-          const character = JSON.parse(body);
-          console.log(character.name);
+          const characterName = JSON.parse(charBody).name;
+          console.log(characterName);
         }
       });
     });
