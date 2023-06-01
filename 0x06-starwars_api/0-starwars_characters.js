fdfb29b5b1b27ecd2dc1bc4d363c50d968a5b541
Oluwajuwon165 <Oladitioluwajuwon165@gmail.com>
2023-06-01 17:40:51 +0100

    Hot fix

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -5,24 +5,18 @@ const filmID = process.argv[2];
 
 async function starwarsCharacters(filmId) {
   const endpoint = 'https://swapi-api.hbtn.io/api/films/' + filmId;
-
   // Send a request to the API endpoint and await the response
   let response = await (await request(endpoint)).body;
-
   // Parse the response as JSON
   response = JSON.parse(response);
-
   const characters = response.characters;
 
   for (let i = 0; i < characters.length; i++) {
     const urlCharacter = characters[i];
-
     // Send a request to the character URL and await the response
     let character = await (await request(urlCharacter)).body;
-
     // Parse the response as JSON
     character = JSON.parse(character);
-
     console.log(character.name);
   }
 }
