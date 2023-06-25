92252924b7fb69b137bf130b6e23ed6f7dca005d
Oluwajuwon165 <Oladitioluwajuwon165@gmail.com>
2023-06-25 19:20:43 +0100

    Island Perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,31 +1,16 @@
 #!/usr/bin/python3
-
+"""
+Island Perimeter
+"""
 
 def island_perimeter(grid):
     """
-    Calculates the perimeter of an island described in grid.
-
-    Args:
-        grid (list): A list of list of integers representing the island.
-
-    Returns:
-        int: The perimeter of the island.
-
+     returns the perimeter of the island described in grid
+    :param grid:
+    :return:
     """
-    if not grid or not grid[0]:
-        return 0
-
-    rows = len(grid)
-    cols = len(grid[0])
-    perimeter = 0
-
-    for i in range(rows):
-        for j in range(cols):
-            if grid[i][j] == 1:
-                perimeter += 4
-                if i > 0 and grid[i - 1][j] == 1:
-                    perimeter -= 2
-                if j > 0 and grid[i][j - 1] == 1:
-                    perimeter -= 2
-
-    return perimeter
+    area = 0
+    for row in grid + list(map(list, zip(*grid))):
+        for i1, i2 in zip([0] + row, row + [0]):
+            area += int(i1 != i2)
+    return area
